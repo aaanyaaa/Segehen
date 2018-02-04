a2679fd2e3b8922864985482e992b4632b633473
LawerenceLee <lucifitz.edward@gmail.com>
2018-02-04 02:52:46 -0800

    initial commit, includes year and date search

---
+++ sagehen_pandas.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import sqlite3
+
+
+DATEBASE = sqlite3.connect("sagehen.db")
+
+
+def year_search(start_yr, end_yr):
+    """
+    Returns a Pandas DataFrame with rows matching the specified year range
+    """
+    return pd.read_sql(
+        "SELECT * FROM hourdata WHERE date_time >= '{}-01-01' AND date_time <= '{}-12-31';".format(start_yr, end_yr),
+        con=DATEBASE
+    )
+
+
+def date_search(start_date, end_date):
+    """
+    Returns a Pandas DataFrame with rows matching the specified date range
+    """
+    return pd.read_sql(
+        "SELECT * FROM hourdata WHERE date >= '{}' AND date <= '{}';".format(start_date, end_date),
+        con=DATEBASE
+    )
