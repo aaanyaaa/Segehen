6f89f7eff1630d8b1c26bf0281326fb73cb4ab11
LawerenceLee <lucifitz.edward@gmail.com>
2018-02-04 03:20:43 -0800

    added some preconfiguration to returned dfs

--- sagehen_pandas.py
+++ sagehen_pandas.py
@@ -9,17 +9,25 @@ def year_search(start_yr, end_yr):
     """
     Returns a Pandas DataFrame with rows matching the specified year range
     """
-    return pd.read_sql(
+    results = pd.read_sql(
         "SELECT * FROM hourdata WHERE date_time >= '{}-01-01' AND date_time <= '{}-12-31';".format(start_yr, end_yr),
         con=DATEBASE
     )
+    results.drop('id', axis=1, inplace=True)
+    results.set_index(['date_time'], inplace=True)
+    results = results.apply(pd.to_numeric, args=('coerce',))
+    return results
 
 
 def date_search(start_date, end_date):
     """
     Returns a Pandas DataFrame with rows matching the specified date range
     """
-    return pd.read_sql(
+    results = pd.read_sql(
         "SELECT * FROM hourdata WHERE date_time >= '{}' AND date_time <= '{}';".format(start_date, end_date),
         con=DATEBASE
     )
+    results.drop('id', axis=1, inplace=True)
+    results.set_index(['date_time'], inplace=True)
+    results = results.apply(pd.to_numeric, args=('coerce',))
+    return results
