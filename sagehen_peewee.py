c0b628637e5344274164a0e7b056221ca62249fc
LawerenceLee <lucifitz.edward@gmail.com>
2018-02-04 14:01:41 -0800

    new db w/null values

--- sagehen_peewee.py
+++ sagehen_peewee.py
@@ -1,5 +1,6 @@
 import csv
 import datetime
+import os
 import peewee
 
 
@@ -8,77 +9,77 @@ DATABASE = peewee.SqliteDatabase('sagehen.db')
 
 class HourData(peewee.Model):
     date_time = peewee.DateTimeField()
-    solar_rad = peewee.CharField(
+    solar_rad = peewee.FloatField(
         verbose_name="TOTAL SOLAR RAD (K W-hr/m^2)",
-        max_length=225,
+        null=True,
     )
-    wind_ave = peewee.CharField(
+    wind_ave = peewee.FloatField(
         verbose_name="AVE WIND SPEED (m/s)",
-        max_length=225,
+        null=True,
     )
-    wind_dir = peewee.CharField(
+    wind_dir = peewee.IntegerField(
         verbose_name="V. WIND DIR (Deg)",
-        max_length=225,
+        null=True,
     )
-    wind_max = peewee.CharField(
+    wind_max = peewee.FloatField(
         verbose_name="MAX WIND SPEED (m/s)",
-        max_length=225,
+        null=True,
     )
-    temp_ave = peewee.CharField(
+    temp_ave = peewee.FloatField(
         verbose_name="AIR TEMP AVE (Deg C)",
-        max_length=225,
+        null=True,
     )
-    temp_max = peewee.CharField(
+    temp_max = peewee.FloatField(
         verbose_name="AIR TEMP MAX (Deg C)",
-        max_length=225,
+        null=True,
     )
-    temp_min = peewee.CharField(
+    temp_min = peewee.FloatField(
         verbose_name="AIR TEMP MIN (Deg C)",
-        max_length=225,
+        null=True,
     )
-    soil_tave = peewee.CharField(
+    soil_tave = peewee.FloatField(
         verbose_name="SOIL TEMP AVE (Deg C)",
-        max_length=225,
+        null=True,
     )
-    soil_tmax = peewee.CharField(
+    soil_tmax = peewee.FloatField(
         verbose_name="SOIL TEMP MAX (Deg C)",
-        max_length=225,
+        null=True,
     )
-    soil_tmin = peewee.CharField(
+    soil_tmin = peewee.FloatField(
         verbose_name="SOIL TEMP MIN (Deg C)",
-        max_length=225,
+        null=True,
     )
-    rh_ave = peewee.CharField(
+    rh_ave = peewee.IntegerField(
         verbose_name="RELATIVE HUMIDITY AVE (%)",
-        max_length=225,
+        null=True,
     )
-    rh_max = peewee.CharField(
+    rh_max = peewee.IntegerField(
         verbose_name="RELATIVE HUMIDITY MAX (%)",
-        max_length=225,
+        null=True,
     )
-    rh_min = peewee.CharField(
+    rh_min = peewee.IntegerField(
         verbose_name="RELATIVE HUMIDITY MIN (%)",
-        max_length=225,
+        null=True,
     )
-    dew_pt = peewee.CharField(
+    dew_pt = peewee.FloatField(
         verbose_name="DEW PT. (Deg C)",
-        max_length=225,
+        null=True,
     )
-    wet_bulb = peewee.CharField(
+    wet_bulb = peewee.FloatField(
         verbose_name="WET BULB (Deg C)",
-        max_length=225,
+        null=True,
     )
-    pressure = peewee.CharField(
+    pressure = peewee.IntegerField(
         verbose_name="BARO. PRESS. (mb)",
-        max_length=225,
+        null=True,
     )
-    snow = peewee.CharField(
+    snow = peewee.FloatField(
         verbose_name="SNOW DEPTH (mm)",
-        max_length=225,
+        null=True,
     )
-    precip = peewee.CharField(
+    precip = peewee.FloatField(
         verbose_name="TOTAL PRECIP (mm)",
-        max_length=225,
+        null=True,
     )
 
     class Meta:
@@ -98,30 +99,49 @@ def build_db():
     with open('SAGEHEN_1APR1997-14DEC2017.csv') as csvfile:
         csv_reader = csv.DictReader(csvfile)
         for row in csv_reader:
-            date_str = row['DATE'] + " " + row['HOUR OF DAY ENDING AT L.S.T']
-            HourData.create(
-                date_time=datetime.datetime.strptime(date_str, "%Y-%m-%d %H:%M"),
-                solar_rad=row["TOTAL SOLAR RAD (K W-hr/m^2)"],
-                wind_ave=row["AVE WIND SPEED (m/s)"],
-                wind_dir=row["V. WIND DIR (Deg)"],
-                wind_max=row["MAX WIND SPEED (m/s)"],
-                temp_ave=row["AIR TEMP AVE (Deg C)"],
-                temp_max=row["AIR TEMP MAX (Deg C)"],
-                temp_min=row["AIR TEMP MIN (Deg C)"],
-                soil_tave=row["SOIL TEMP AVE (Deg C)"],
-                soil_tmax=row["SOIL TEMP MAX (Deg C)"],
-                soil_tmin=row["SOIL TEMP MIN (Deg C)"],
-                rh_ave=row["RELATIVE HUMIDITY AVE (%)"],
-                rh_max=row["RELATIVE HUMIDITY MAX (%)"],
-                rh_min=row["RELATIVE HUMIDITY MIN (%)"],
-                dew_pt=row["DEW PT. (Deg C)"],
-                wet_bulb=row["WET BULB (Deg C)"],
-                pressure=row["BARO. PRESS. (mb)"],
-                snow=row["SNOW DEPTH (mm)"],
-                precip=row["TOTAL PRECIP (mm)"],
-            )
+            Alpha = HourData()
+            date_str = row['DATE'] + " " + row['HOUR OF DAY ENDING AT L.S.T'] + ":00"
+            Alpha.date_time = date_str
+            if row["TOTAL SOLAR RAD (K W-hr/m^2)"] != "":
+                Alpha.solar_rad = float(row["TOTAL SOLAR RAD (K W-hr/m^2)"])
+            if row["AVE WIND SPEED (m/s)"] != "":
+                Alpha.wind_ave = float(row["AVE WIND SPEED (m/s)"])
+            if row["V. WIND DIR (Deg)"] != "":
+                Alpha.wind_dir = int(row["V. WIND DIR (Deg)"])
+            if row["MAX WIND SPEED (m/s)"] != "":
+                Alpha.wind_max = float(row["MAX WIND SPEED (m/s)"])
+            if row["AIR TEMP AVE (Deg C)"] != "":
+                Alpha.temp_ave = float(row["AIR TEMP AVE (Deg C)"])
+            if row["AIR TEMP MAX (Deg C)"] != "":
+                Alpha.temp_max = float(row["AIR TEMP MAX (Deg C)"])
+            if row["AIR TEMP MIN (Deg C)"] != "":
+                Alpha.temp_min = float(row["AIR TEMP MIN (Deg C)"])
+            if row["SOIL TEMP AVE (Deg C)"] != "":
+                Alpha.soil_tave = float(row["SOIL TEMP AVE (Deg C)"])
+            if row["SOIL TEMP MAX (Deg C)"] != "":
+                Alpha.soil_tmax = float(row["SOIL TEMP MAX (Deg C)"])
+            if row["SOIL TEMP MIN (Deg C)"] != "":
+                Alpha.soil_tmin = float(row["SOIL TEMP MIN (Deg C)"])
+            if row["RELATIVE HUMIDITY AVE (%)"] != "":
+                Alpha.rh_ave = int(row["RELATIVE HUMIDITY AVE (%)"])
+            if row["RELATIVE HUMIDITY MAX (%)"] != "":
+                Alpha.rh_max = int(row["RELATIVE HUMIDITY MAX (%)"])
+            if row["RELATIVE HUMIDITY MIN (%)"] != "":
+                Alpha.rh_min = int(row["RELATIVE HUMIDITY MIN (%)"])
+            if row["DEW PT. (Deg C)"] != "":
+                Alpha.dew_pt = float(row["DEW PT. (Deg C)"])
+            if row["WET BULB (Deg C)"] != "":
+                Alpha.wet_bulb = float(row["WET BULB (Deg C)"])
+            if row["BARO. PRESS. (mb)"] != "":
+                Alpha.pressure = int(row["BARO. PRESS. (mb)"])
+            if row["SNOW DEPTH (mm)"] != "":
+                Alpha.snow = float(row["SNOW DEPTH (mm)"])
+            if row["TOTAL PRECIP (mm)"] != "":
+                Alpha.precip = float(row["TOTAL PRECIP (mm)"])
+            Alpha.save()
 
 
 if __name__ == "__main__":
     connect_and_create_tables()
     build_db()
+    os.system("say 'Built Database'")
