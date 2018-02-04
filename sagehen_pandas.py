e4fab23f650e6ed88017aa954e67b8fd9db3f3dd
LawerenceLee <lucifitz.edward@gmail.com>
2018-02-04 14:01:58 -0800

    added value_search function

--- sagehen_pandas.py
+++ sagehen_pandas.py
@@ -31,3 +31,18 @@ def date_search(start_date, end_date):
     results.set_index(['date_time'], inplace=True)
     results = results.apply(pd.to_numeric, args=('coerce',))
     return results
+
+
+def value_search(col, start_val, end_val):
+    """
+    Returns a Pandas DataFrame with the rows matching the specified value range within 
+    the specified column
+    """
+    results = pd.read_sql(
+        "SELECT * FROM hourdata WHERE {} >= {} AND {} <= {};".format(col, start_val, col, end_val),
+        con=DATEBASE
+    )
+    results.drop('id', axis=1, inplace=True)
+    results.set_index(['date_time'], inplace=True)
+    results = results.apply(pd.to_numeric, args=('coerce',))
+    return results
