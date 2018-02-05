8ec6c84357ef6b08fd97eb0af82a10215e88674f
LawerenceLee <lucifitz.edward@gmail.com>
2018-02-04 16:51:44 -0800

    moved dataframe configuration into a function

--- sagehen_pandas.py
+++ sagehen_pandas.py
@@ -5,6 +5,20 @@ import sqlite3
 DATEBASE = sqlite3.connect("sagehen.db")
 
 
+def config_df(dataframe_obj):
+    """
+    Configures a Pandas DataFrame to a set of specifications
+    """
+    dataframe_obj.drop('id', axis=1, inplace=True)
+    dataframe_obj['date_time'] = pd.to_datetime(
+        dataframe_obj['date_time'],
+        errors="coerce",
+    )
+    dataframe_obj.set_index(['date_time'], inplace=True)
+    dataframe_obj = dataframe_obj.apply(pd.to_numeric, args=('coerce',))
+    return dataframe_obj
+
+
 def year_search(start_yr, end_yr):
     """
     Returns a Pandas DataFrame with rows matching the specified year range
@@ -13,10 +27,7 @@ def year_search(start_yr, end_yr):
         "SELECT * FROM hourdata WHERE date_time >= '{}-01-01' AND date_time <= '{}-12-31';".format(start_yr, end_yr),
         con=DATEBASE
     )
-    results.drop('id', axis=1, inplace=True)
-    results.set_index(['date_time'], inplace=True)
-    results = results.apply(pd.to_numeric, args=('coerce',))
-    return results
+    return config_df(results)
 
 
 def date_search(start_date, end_date):
@@ -27,22 +38,17 @@ def date_search(start_date, end_date):
         "SELECT * FROM hourdata WHERE date_time >= '{}' AND date_time <= '{}';".format(start_date, end_date),
         con=DATEBASE
     )
-    results.drop('id', axis=1, inplace=True)
-    results.set_index(['date_time'], inplace=True)
-    results = results.apply(pd.to_numeric, args=('coerce',))
-    return results
+    return config_df(results)
 
 
 def value_search(col, start_val, end_val):
     """
-    Returns a Pandas DataFrame with the rows matching the specified value range within 
-    the specified column
+    Returns a Pandas DataFrame with the rows matching the specified value
+    range within the specified column
     """
     results = pd.read_sql(
-        "SELECT * FROM hourdata WHERE {} >= {} AND {} <= {};".format(col, start_val, col, end_val),
+        "SELECT * FROM hourdata WHERE {} >= {} AND {} <= {} AND {} IS NOT NULL;".format(col, start_val, col, end_val, col),
         con=DATEBASE
     )
-    results.drop('id', axis=1, inplace=True)
-    results.set_index(['date_time'], inplace=True)
-    results = results.apply(pd.to_numeric, args=('coerce',))
-    return results
+    return config_df(results)
+
